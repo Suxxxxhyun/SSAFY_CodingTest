9ae5310c2f5e51ff97a6c92c870c786019c3d88d
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2023-10-17 00:07:32 +0900

    231016 - 코드트리  3문제

---
+++ MyeongIn/ct/Simulation_떨어지는_1자_블록.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Simulation_떨어지는_1자_블록 {
+    public static Scanner sc = new Scanner(System.in);
+    public static int n = sc.nextInt();
+    public static int[][] arr = new int[n][n];
+    // public static int[][] temp = new int[n][n];
+    public static int m = sc.nextInt();
+    public static int k = sc.nextInt() - 1;
+
+    public static void simul(int size, int col) {
+
+        int findR = n - 1;
+        boolean find = false;
+        for (int i = 0; i < n; i++) {
+            if (find)
+                break;
+            for (int j = k; j <= k + m - 1; j++) {
+                if (arr[i][j] == 1) {
+                    findR = i - 1;
+                    find = true;
+                }
+            }
+        }
+
+        for (int j = k; j <= k + m - 1; j++) {
+            arr[findR][j] = 1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        simul(m, k);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
