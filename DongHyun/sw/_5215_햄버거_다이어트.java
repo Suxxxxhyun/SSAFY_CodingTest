fff9026b4b5cb8b09a046bde377970264e018d3b
706com <ehdgusjdbswl@gmail.com>
2023-11-07 12:38:40 +0900

    231107 - SWEA.5215 햄버거 다이어트 (복습)

--- DongHyun/sw/_5215_햄버거_다이어트.java
+++ DongHyun/sw/_5215_햄버거_다이어트.java
@@ -9,10 +9,10 @@ package saffy.DongHyun.sw;
 // 2. DP 를 이용하여, 가장 최고의 점수를 만들 수 있는 것을 찾는다.
 // -> DP 의 역할 : 칼로리를 뜻하는 인덱스 , 인덱스 값 : 그 칼로리 안에서 점수 중 가장 높은 값.
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 import java.util.StringTokenizer;
 
 public class _5215_햄버거_다이어트 {
@@ -43,7 +43,7 @@ public class _5215_햄버거_다이어트 {
                     if(kcal[i] > j){
                         dp[i][j] = dp[i-1][j];
                     } else{
-                        dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-kcal[i]] + score[i]);
+                        dp[i][j] = Math.max(dp[i-1][j],score[i]+dp[i-1][j-kcal[i]]);
                     }
                 }
             }
